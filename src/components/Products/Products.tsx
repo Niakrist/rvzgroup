c7b2c61985281b26a9fb944762eb674dd2c1814e
Niakrist <ak_88dron@inbox.ru>
2025-02-08 14:20:50 +0300

    feat: emplement Products.tsx

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,8 +1,21 @@
 import React from "react";
-// import styles from "./Products.module.css";
+import { products } from "../PopularProduct/PopularProduct.props";
+import styles from "./Products.module.css";
+import { IProduct } from "@/types/product";
+import { ProductItem } from "../ProductItem/ProductItem";
 
 const Products = () => {
-  return <div style={{ border: "1px solid black" }}>123</div>;
+  return (
+    <>
+      <ul className={styles.list}>
+        {products.map((product: IProduct) => (
+          <li key={product.article}>
+            <ProductItem product={product} />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default Products;
