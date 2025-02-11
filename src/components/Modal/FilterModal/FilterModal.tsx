ab5b8c878b1f6079d55ccc8cd11b5f4dfec79e02
Niakrist <ak_88dron@inbox.ru>
2025-02-11 15:20:22 +0300

    fix: delete empty import

--- src/components/Modal/FilterModal/FilterModal.tsx
+++ src/components/Modal/FilterModal/FilterModal.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect } from "react";
+import React from "react";
 import { Modal } from "../Modal";
 import { Filter } from "@/components/Filter/Filter";
 import { toggleOpenMobileFilter } from "@/store/openModalSlice/openModalSlice";
