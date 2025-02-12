02ff1b4a993f07d987ebe33f9fb308e9b2d83bf6
Niakrist <ak_88dron@inbox.ru>
2025-02-12 12:55:27 +0300

    add scrollbar in mobile burger menu

--- src/components/Menu/MenuBurger/MenuBurger.tsx
+++ src/components/Menu/MenuBurger/MenuBurger.tsx
@@ -31,86 +31,82 @@ export const MenuBurger = () => {
   return (
     <Modal width="mobileMenu">
       <button onClick={handleCloseModal} className={styles.close} />
-      <div className={styles.wrapper}>
-        {isOpenMobileCatalog ? (
-          <>
-            <button onClick={toggleMobileCatalog} className={styles.back}>
-              <Icon className={styles.iconArrowDown} name="iconArrowDown" />{" "}
-              Назад
-            </button>
-            <nav>
-              <ul className={styles.list}>
-                {menuCatalogList.map((item) => (
-                  <li key={item.url}>
-                    <Link className={styles.linkCatalog} href={`.${item.url}`}>
-                      <img
-                        className={styles.images}
-                        src={`./icons/${item.img}`}
-                        alt=""
-                      />{" "}
-                      {item.name}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </nav>
-          </>
-        ) : (
-          <>
-            <nav className={styles.nav}>
-              <ul className={styles.list}>
-                <li>
-                  <Link className={styles.link} href="/">
-                    Главная
-                  </Link>
-                </li>
-                <li>
-                  <button
-                    onClick={toggleMobileCatalog}
-                    className={styles.catalog}>
-                    Каталог{" "}
-                    <Icon
-                      className={styles.iconArrowDown}
-                      name="iconArrowDown"
+      {/* <div className={styles.wrapper}> */}
+      {isOpenMobileCatalog ? (
+        <>
+          <button onClick={toggleMobileCatalog} className={styles.back}>
+            <Icon className={styles.iconArrowDown} name="iconArrowDown" /> Назад
+          </button>
+          <nav>
+            <ul className={styles.list}>
+              {menuCatalogList.map((item) => (
+                <li key={item.url}>
+                  <Link className={styles.linkCatalog} href={`.${item.url}`}>
+                    <img
+                      className={styles.images}
+                      src={`./icons/${item.img}`}
+                      alt=""
                     />{" "}
-                  </button>
-                </li>
-                <li>
-                  <Link className={styles.link} href="/about">
-                    О компании
+                    {item.name}
                   </Link>
                 </li>
-                <li>
-                  <Link className={styles.link} href="/brand-rvz">
-                    Бренд РВЗ
-                  </Link>
-                </li>
-                <li>
-                  <Link className={styles.link} href="delivery">
-                    Доставка и оплата
-                  </Link>
-                </li>
-                <li>
-                  <Link className={styles.link} href="/vacancy">
-                    Вакансии
-                  </Link>
-                </li>
-                <li>
-                  <Link className={styles.link} href="/contacts">
-                    Контакты
-                  </Link>
-                </li>
-              </ul>
-            </nav>
+              ))}
+            </ul>
+          </nav>
+        </>
+      ) : (
+        <>
+          <nav className={styles.nav}>
+            <ul className={styles.list}>
+              <li>
+                <Link className={styles.link} href="/">
+                  Главная
+                </Link>
+              </li>
+              <li>
+                <button
+                  onClick={toggleMobileCatalog}
+                  className={styles.catalog}>
+                  Каталог{" "}
+                  <Icon className={styles.iconArrowDown} name="iconArrowDown" />{" "}
+                </button>
+              </li>
+              <li>
+                <Link className={styles.link} href="/about">
+                  О компании
+                </Link>
+              </li>
+              <li>
+                <Link className={styles.link} href="/brand-rvz">
+                  Бренд РВЗ
+                </Link>
+              </li>
+              <li>
+                <Link className={styles.link} href="delivery">
+                  Доставка и оплата
+                </Link>
+              </li>
+              <li>
+                <Link className={styles.link} href="/vacancy">
+                  Вакансии
+                </Link>
+              </li>
+              <li>
+                <Link className={styles.link} href="/contacts">
+                  Контакты
+                </Link>
+              </li>
+            </ul>
+          </nav>
 
-            <address className={styles.address}>
-              <a href="mailto:sales@rvzgroup.ru">sales@rvzgroup.ru</a>
-              <a href="tel:78126428527">+7 812 642-85-27</a>
-              <p>Санкт-Петербург, улица Ржевская, д. 4</p>
-            </address>
-          </>
-        )}
-      </div>
+          <address className={styles.address}>
+            <a href="mailto:sales@rvzgroup.ru">sales@rvzgroup.ru</a>
+            <a href="tel:78126428527">+7 812 642-85-27</a>
+            <p>Санкт-Петербург, улица Ржевская, д. 4</p>
+          </address>
+        </>
+      )}
+      {/* </div> */}
     </Modal>
   );
 };
