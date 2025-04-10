9f1344399f5a344db13008e99c5211e12e9c551a
Niakrist <ak_88dron@inbox.ru>
2025-04-10 11:34:36 +0300

    fix CartPage

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { Metadata } from "next";
 import { Cart, PopularProduct } from "@/components";
+import { getCategories } from "@/api/getCategories";
 
 export const metadata: Metadata = {
   title: "Подшипник 204",
   description: "Купить подшипник 204",
 };
 
-export default function CartPage() {
+export default async function CartPage() {
+  const products = await getCategories();
   return (
     <>
       <Cart />
-      <PopularProduct />
+      <PopularProduct products={products.rows} />
     </>
   );
 }
