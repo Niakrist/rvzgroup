f2775ea651d309fb797e2b5b7cb90b5116455f22
Niakrist <ak_88dron@inbox.ru>
2025-04-10 10:43:09 +0300

    clean empty import

--- src/components/CounterQuantity/CounterQuantity.props.ts
+++ src/components/CounterQuantity/CounterQuantity.props.ts
@@ -1,9 +1,9 @@
 export interface ICounterQuantityProps {
   bgColor: "white" | "grey";
   size: "medium" | "large";
-  handleChangeQuantity: any;
-  handleIncrement: any;
-  handleDecrement: any;
+  handleChangeQuantity: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleIncrement: () => void;
+  handleDecrement: () => void;
   count: number;
   className?: string;
 }
