495f5b2d2c4bf6b5841913089e9b64a1aebb8bbf
Niakrist <ak_88dron@inbox.ru>
2025-06-06 16:19:22 +0300

    update GetPriceModal

--- src/components/Modal/GetPriceModal/GetPriceModal.tsx
+++ src/components/Modal/GetPriceModal/GetPriceModal.tsx
@@ -16,7 +16,7 @@ export const GetPriceModal = () => {
   const [phone, setPhone] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [textAreaValue, setTextAreaValue] = useState<string>("");
-  const [file, setFile] = useState(null);
+  const [file, setFile] = useState<File | null>(null);
 
   const dispatch = useDispatch();
   const { getPriceModal } = useSelector((state: RootState) => state.openModal);
@@ -44,8 +44,10 @@ export const GetPriceModal = () => {
     setIsCheck(!check);
   };
 
-  const handleFileChange = (e) => {
-    setFile(e.target.files[0]);
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setFile(e.target.files[0]);
+    }
   };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
