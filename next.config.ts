c0b1d896ebd23fd2be28211a9aa09d0a403370ca
Niakrist <ak_88dron@inbox.ru>
2025-06-25 11:00:29 +0300

    fix next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -9,25 +9,6 @@ const nextConfig: NextConfig = {
       },
     ];
   },
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          { type: "host", value: "rvzgroup.ru" },
-          { type: "header", key: "x-forwarded-proto", value: "http" },
-        ],
-        destination: "https://rvzgroup.ru/:path*",
-        permanent: true,
-      },
-      {
-        source: "/:path*",
-        has: [{ type: "host", value: "www.rvzgroup.ru" }],
-        destination: "https://rvzgroup.ru/:path*",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
