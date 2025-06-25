4bddbf1d2493fcd4928ac465e3e50ada7bcc199e
Niakrist <ak_88dron@inbox.ru>
2025-06-25 10:30:12 +0300

    fix config

--- next.config.ts
+++ next.config.ts
@@ -4,22 +4,24 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: "/images/:path*",
-        destination: "http://localhost:4000/images/:path*",
+        source: "/images/:path*", // Захватываем все запросы к /images/
+        destination: "http://localhost:4000/:path*", // Перенаправляем их на бэкенд
       },
     ];
   },
   async redirects() {
     return [
+      // Редирект HTTP → HTTPS (только если запрос НЕ HTTPS)
       {
         source: "/:path*",
         has: [
           { type: "host", value: "rvzgroup.ru" },
-          { type: "header", key: "x-forwarded-proto", value: "http" },
+          { type: "header", key: "x-forwarded-proto", value: "http" }, // Проверяем HTTP
         ],
         destination: "https://rvzgroup.ru/:path*",
         permanent: true,
       },
+      // Редирект WWW → без WWW (для любых протоколов)
       {
         source: "/:path*",
         has: [{ type: "host", value: "www.rvzgroup.ru" }],
