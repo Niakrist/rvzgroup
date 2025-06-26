649da6c618969cf8d0f591c8a5494cf66e6d521b
Niakrist <ak_88dron@inbox.ru>
2025-06-26 13:13:10 +0300

    update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,18 +6,42 @@ import {
 } from "@/constants/sitemap";
 import { MetadataRoute } from "next";
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  const staticSitemap = [
-    ...sitemapServicesPage,
-    ...sitamapCategory,
-    ...sitemapSpecifications,
-    ...sitemapBearings,
-  ].map((url) => ({
-    url: `https://rvzgroup.ru/${url}`,
-    lastModified: new Date(),
-    changeFrequency: "daily" as const,
-    priority: 1,
-  }));
+export const revalidate = 86400; // Обновлять раз в сутки
 
-  return staticSitemap;
+export default function sitemap(): MetadataRoute.Sitemap {
+  const sitemapEntries = [
+    // Главные страницы
+    ...sitemapServicesPage.map((url) => ({
+      url: `https://rvzgroup.ru/${url}`,
+      lastModified: new Date(),
+      changeFrequency: "daily" as const,
+      priority: 1.0,
+    })),
+
+    // Категории
+    ...sitamapCategory.map((url) => ({
+      url: `https://rvzgroup.ru/${url}`,
+      lastModified: new Date(),
+      changeFrequency: "weekly" as const,
+      priority: 0.8,
+    })),
+    // Категории фильтров
+    ...sitemapSpecifications.map((url) => ({
+      url: `https://rvzgroup.ru/${url}`,
+      lastModified: new Date(),
+      changeFrequency: "weekly" as const,
+      priority: 0.8,
+    })),
+
+    // Товары
+    ...sitemapBearings.map((url) => ({
+      url: `https://rvzgroup.ru/${url}`,
+      lastModified: new Date(),
+      changeFrequency: "daily" as const,
+      priority: 1,
+    })),
+  ];
+
+  console.log(`Sitemap generated with ${sitemapEntries.length} URLs`);
+  return sitemapEntries;
 }
