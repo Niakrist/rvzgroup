761ab897f18506d4526672a0cade17e266bd4a35
Niakrist <ak_88dron@inbox.ru>
2025-10-10 09:37:23 +0300

    fix IconCheckbox

--- src/components/Icon/icons/IconCheckbox.tsx
+++ src/components/Icon/icons/IconCheckbox.tsx
@@ -8,8 +8,8 @@ const IconCheckbox: React.FC<IIconCheckboxProps> = ({ className }) => {
   return (
     <svg
       className={className}
-      width="0"
-      height="0"
+      width="10"
+      height="8"
       viewBox="0 0 10 8"
       fill="none"
       xmlns="http://www.w3.org/2000/svg">
