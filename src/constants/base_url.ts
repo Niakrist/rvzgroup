6728649f309d5677aa2ce9e9f941cbe441298e88
Niakrist <ak_88dron@inbox.ru>
2025-12-25 15:31:28 +0300

    change base_url

--- src/constants/base_url.ts
+++ src/constants/base_url.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://31.207.75.168:4000/api/v1";
+export const BASE_URL = "http://155.212.184.175:4000/api/v1";
 
 // export const BASE_URL = "http://localhost:4000/api/v1";
