1813016166bb7242170d37b5c9c84e3516b3b34d
Niakrist <ak_88dron@inbox.ru>
2026-01-28 13:18:51 +0300

    fix next.config

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,14 @@
 import type { NextConfig } from "next";
 
-const isProduction = process.env.NODE_ENV === "production";
-const apiUrl = isProduction ? "http://localhost:4000" : "http://localhost:4000";
-
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: "/api/:path*",
-        destination: `${apiUrl}/:path*`,
-      },
-      {
         source: "/images/:path*",
-        destination: `${apiUrl}/:path*`,
+        destination: "http://localhost:4000/:path*",
       },
     ];
   },
-  output: "standalone",
 };
 
 export default nextConfig;
