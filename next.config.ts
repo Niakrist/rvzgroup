827897f66940162564882162412203eb47ee4a33
Niakrist <ak_88dron@inbox.ru>
2026-01-28 10:59:56 +0300

    change next config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,22 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === "production";
+const apiUrl = isProduction ? "http://localhost:4000" : "http://localhost:4000";
+
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
+        source: "/api/:path*",
+        destination: `${apiUrl}/:path*`,
+      },
+      {
         source: "/images/:path*",
-        destination: "http://localhost:4000/:path*",
+        destination: `${apiUrl}/:path*`,
       },
     ];
   },
+  output: "standalone",
 };
 
 export default nextConfig;
