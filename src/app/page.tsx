288c1c3cc4af7625420cf1684180862182a6bec4
Niakrist <ak_88dron@inbox.ru>
2026-06-10 20:06:40 +0300

    optimization 7

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,18 +11,10 @@ import {
 
 import dynamic from "next/dynamic";
 
-const AboutCompany = dynamic(() => import("@/components").then((mod) => mod.AboutCompany), {
-  ssr: false,
-});
-const Advantage = dynamic(() => import("@/components").then((mod) => mod.Advantage), {
-  ssr: false,
-});
-const WeWork = dynamic(() => import("@/components").then((mod) => mod.WeWork), {
-  ssr: false,
-});
-const Subscribe = dynamic(() => import("@/components").then((mod) => mod.Subscribe), {
-  ssr: false,
-});
+const AboutCompany = dynamic(() => import("@/components").then((mod) => mod.AboutCompany));
+const Advantage = dynamic(() => import("@/components").then((mod) => mod.Advantage));
+const WeWork = dynamic(() => import("@/components").then((mod) => mod.WeWork));
+const Subscribe = dynamic(() => import("@/components").then((mod) => mod.Subscribe));
 
 export default async function Home() {
   const paramsForPopularProduct = new URLSearchParams({
